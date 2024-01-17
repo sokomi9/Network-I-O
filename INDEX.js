8d769fd18e28a3b2352de1382ed31b1377cd5dce
Abiud Sokomi <abiudsokomi@gmail.com>
2024-01-17 14:16:46 +0300

    my commit

---
+++ INDEX.js
@@ -0,0 +1,22 @@
+const https = require("node:https");
+
+// const crypto = require("node:crypto");
+
+// process.env.UV_THREADPOOL_SIZE = 16;
+const MAX_CALLS = 15;
+
+const start = Date.now();
+for (let i = 0; i<MAX_CALLS; i++){
+    https
+    .request("https://www.google.com", (res) => {
+        res.on("data", () => {});
+        res.on("end", () => {
+            console.log(`Request: ${i + 1}`, Date.now() - start);
+        });
+    } )
+    .end();
+    // crypto.pbkdf2("password", "salt", 100000, 512, "sha512", ()=> {
+    //     console.log(`Hash: ${i + 1}`, Date.now() - start);
+    // });
+}
+console.log("Has: ", Date.now() - start);
